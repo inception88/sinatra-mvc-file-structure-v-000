fd751069690af92ddc3bf180e0f2ee25e0e36025
Benjamin Jones <bljones36@gmail.com>
2018-12-21 16:47:17 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,12 @@
 class Dog
 
+  @@all = []
   attr_accessor :name, :breed, :age
 
   def initialization(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    @@all < self
   end
 end
